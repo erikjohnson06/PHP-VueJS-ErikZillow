cce05d34f8f52848b2b96fca72abc1c8c140b21f
Erik Johnson <erikjohnson06@gmail.com>
2023-03-10 05:31:10 -0500

    Initial Commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <Link href="/">Main Page</Link> &nbsp;
+    <Link href="/show">Show Page</Link>
+
+    <div>Timer:  {{ timer }}</div>
+
+    <slot>Default</slot>
+</template>
+
+<script setup>
+
+    import {Link} from '@inertiajs/vue3'
+    import {ref} from 'vue'
+
+    const timer = ref(0);
+
+    setInterval(() => timer.value++, 1000);
+
+</script>
